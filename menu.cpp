bbf0d31c67ca8f2ecea3f115da5ad250a9aa25fb
dickyhermawan12 <dickyrahmahermawan@gmail.com>
2020-05-20 16:13:39 +0700

    menambah constructor struct

--- menu.cpp
+++ menu.cpp
@@ -50,7 +50,11 @@ struct pakaian {
     int jenis;
     int warna;
     float berat;
-    pakaian* next = NULL;
+    pakaian* next;
+
+    pakaian(){
+        next = NULL;
+    }
 
     void printJenis(){
         cout << "Jenis pakaian yang diterima:\n"
@@ -88,11 +92,18 @@ struct dataPenerimaan {
     string namaCust;
     string alamat;
     int paket;
-    float beratTotal=0;
+    float beratTotal;
     int jumlahPakaian;
-    double total=0;
-    pakaian* listPakaian = NULL;
-    dataPenerimaan* next = NULL;
+    double total;
+    pakaian* listPakaian;
+    dataPenerimaan* next;
+
+    dataPenerimaan(){
+        beratTotal = 0;
+        total = 0;
+        listPakaian = NULL;
+        next = NULL;
+    }
 
     void countTotal(float tempBerat, int tempPaket){
         int biaya;
@@ -226,71 +237,70 @@ void menuAntrianPesanan(dataPenerimaan *&antrianPelanggan){
     }
 }
 
-class menu {
-    private:
-        int menuSelector;
-        dataPenerimaan* antrianPelanggan;
-        enum menuEnum
-        {
-            TAMBAHPESANAN=1,
-            ATURMESINCUCI,
-            ATURSETRIKA,
-            SIMPANRAK,
-            AMBILPESANAN,
-            KONFIGURASI,
-            KELUAR
-        };
-    public:
-        menu(){
-            menuSelector = 0;
-            antrianPelanggan = NULL;
-        }
+struct menu {
+    int menuSelector;
+    dataPenerimaan* antrianPelanggan;
+    enum menuEnum
+    {
+        TAMBAHPESANAN=1,
+        ATURMESINCUCI,
+        ATURSETRIKA,
+        SIMPANRAK,
+        AMBILPESANAN,
+        KONFIGURASI,
+        KELUAR
+    };
+    
+    menu(){
+        menuSelector = 0;
+        antrianPelanggan = NULL;
+    }
 
-        void display(){
-            while (true){
-                system("cls");
-                printEqualSign(37);
-                cout << "          LAUNDRY CUAN-CUAN" << endl;
-                printEqualSign(37);
-                cout << "Menu\n"
-                        "1. Antrian Pesanan\n"
-                        "2. Atur Mesin Cuci\n"
-                        "3. Atur Setrikaan\n"
-                        "4. Simpan ke Rak\n"
-                        "5. Ambil Pesanan\n"
-                        "6. Konfigurasi\n"
-                        "7. Keluar\n";
-                printEqualSign(37);
-                cout << "Pilih Menu > ";
-                cin >> menuSelector;
-                switch(menuSelector){
-                    case TAMBAHPESANAN:
-                        menuAntrianPesanan(antrianPelanggan);
-                        break;
-                    case ATURMESINCUCI:
-                        cout << "Mengatur Mesin Cuci" << endl;
-                        break;
-                    case ATURSETRIKA:
-                        cout << "Mengatur Setrika" << endl;
-                        break;
-                    case SIMPANRAK:
-                        cout << "Menyimpan ke Rak" << endl;
-                        break;
-                    case AMBILPESANAN:
-                        cout << "Mengambil Pesanan" << endl;
-                        break;
-                    case KONFIGURASI:
-                        cout << "Memulai Konfigurasi" << endl;
-                        break;
-                    case KELUAR:
-                        cout << "Sampai Jumpa Lagi!" << endl;
-                        exit(1);
-                    default:
-                        cout << "Masukan Anda salah!" << endl;
-                }
-                system("pause");
+    void display(){
+        while (true){
+            system("cls");
+            printEqualSign(37);
+            cout << "          LAUNDRY CUAN-CUAN" << endl;
+            printEqualSign(37);
+            cout << "Menu\n"
+                    "1. Antrian Pesanan\n"
+                    "2. Atur Mesin Cuci\n"
+                    "3. Atur Setrikaan\n"
+                    "4. Simpan ke Rak\n"
+                    "5. Ambil Pesanan\n"
+                    "6. Konfigurasi\n"
+                    "7. Keluar\n";
+            printEqualSign(37);
+            cout << "Pilih Menu > ";
+            cin >> menuSelector;
+            switch(menuSelector){
+                case TAMBAHPESANAN:
+                    menuAntrianPesanan(antrianPelanggan);
+                    break;
+                case ATURMESINCUCI:
+                    cout << "Mengatur Mesin Cuci" << endl;
+                    break;
+                case ATURSETRIKA:
+                    cout << "Mengatur Setrika" << endl;
+                    break;
+                case SIMPANRAK:
+                    cout << "Menyimpan ke Rak" << endl;
+                    break;
+                case AMBILPESANAN:
+                    cout << "Mengambil Pesanan" << endl;
+                    break;
+                case KONFIGURASI:
+                    cout << "Memulai Konfigurasi" << endl;
+                    break;
+                case KELUAR:
+                    cout << "Sampai Jumpa Lagi!" << endl;
+                    exit(1);
+                default:
+                    cout << "Masukan Anda salah!" << endl;
             }
+            system("pause");
         }
+    }
 } mainMenu;
 
 int main(){
